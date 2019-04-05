7431fa5aa8b67e8562ea217e50ef9588e17c3d7b
SamMatt87 <sam.matthews.77@gmail.com>
2019-04-06 06:37:16 +1100

    movies project update

--- movies/movies_kmeans.py
+++ movies/movies_kmeans.py
@@ -7,11 +7,11 @@ from sklearn.cluster import KMeans
 from anonymised import anonymiser
 import anonymised
 from vectorizer import vectorised
+from visualiser import visualised
 
 out, vectorizer = vectorised()
-visualizer = FreqDistVisualizer(vectorizer.get_feature_names(),n=10)
-visualizer.fit(out)
-visualizer.poof()
+visualised(out,vectorizer)
+
 model = KMeans(n_clusters = 10, init='k-means++', max_iter = 100, n_init=1)
 model.fit(out)
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
